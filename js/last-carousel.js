24f1d373cded8506371f38d1725bfdb01da0702c
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-08-01 21:20:40 +0300

    added img

---
+++ js/last-carousel.js
@@ -0,0 +1,11 @@
+// const DOM = {
+//   <div class="last__carousel-images">
+//   <img src="./images/last_carousel/baked1.png" alt="baked1" class="last__carousel-img">
+//   <img src="./images/last_carousel/baked2.png" alt="baked2" class="last__carousel-img">
+//   <img src="./images/last_carousel/baked3.png" alt="baked3" class="last__carousel-img">
+//   <img src="./images/last_carousel/baked4.png" alt="baked4" class="last__carousel-img">
+//   <img src="./images/last_carousel/baked5.png" alt="baked5" class="last__carousel-img">
+// </div>
+// }
+
+CAROUSEL
