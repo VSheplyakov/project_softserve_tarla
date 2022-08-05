bcb8ff9dc45bbb59b90eb5c887dd519243209df2
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-08-05 22:54:12 +0300

    edited first carousel.js and css + add img

--- js/first-caroucel.js
+++ js/first-caroucel.js
@@ -1,36 +1,38 @@
 (function () {
   const slides = [
-    '<div class="carouselImg"><img class ="carouselImg" src="images/first_carousel/croiddant_carousel.png" alt="carousel"></div>',
-    '<div class="carouselImg"><img class ="carouselImg" src="images/first_carousel/croiddant_carousel1.png" alt = "Almond"></div>',
-    '<div class="carouselImg"><img class ="carouselImg" src="images/first_carousel/croiddant_carousel2.png" alt = "Apricot"></div>',
-    '<div class="carouselImg"><img class ="carouselImg" src="images/first_carousel/croiddant_carousel3.png" alt = "Choco"></div>'
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg0.png" alt="carouselImg0">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg1.png" alt="carouselImg1">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg2.png" alt="carouselImg2">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg4.png" alt="carouselImg4">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg5.png" alt="carouselImg5">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg6.png" alt="carouselImg6">`,
+    `<img class ="carouselImg" src="./images/first_carousel/carouselImg7.png" alt="carouselImg7">`,
   ];
 
   let currentSlide = 0;
 
-    function renderCarousel() {
-       const slidepContainer = document.querySelector('.fcarousel .carouselImg');
-       slidepContainer.innerHTML = slides[currentSlide];
-    }
+  function renderCarousel() {
+    const slidepContainer = document.querySelector('.fcarousel .carouselImg');
+    slidepContainer.innerHTML = slides[currentSlide];
+  }
 
-    function nextSlide() {
-        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-        renderCarousel();
-    }
-
-    function prevSlide() {
-        currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
-        renderCarousel();
-    }
-
-    const btnForward = document.querySelector('.fcarousel .forwardp');
-    btnForward.addEventListener('click', nextSlide);
-
-    const btnBack = document.querySelector('.fcarousel .backp');
-    btnBack.addEventListener('click', prevSlide);
-
-    window.addEventListener('resize', renderCarousel);
-
+  function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
     renderCarousel();
+  }
 
+  function prevSlide() {
+    currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
+    renderCarousel();
+  }
+
+  const btnForward = document.querySelector('.fcarousel .forwardp');
+  btnForward.addEventListener('click', nextSlide);
+
+  const btnBack = document.querySelector('.fcarousel .backp');
+  btnBack.addEventListener('click', prevSlide);
+
+  window.addEventListener('resize', renderCarousel);
+
+  renderCarousel();
 })();
