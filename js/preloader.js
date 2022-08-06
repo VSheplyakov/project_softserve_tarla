62b4b24135747747b2403611d2180e560177dddd
DadaLV <vynohradova.l.a@gmail.com>
2022-08-06 12:53:49 +0300

    preloader edited

--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,9 @@
-// setTimeout(() => {
-//   const preload = document.getElementById('preloader');
-//   preload.style.display = 'none';
-// }, 3000);
-
 const preload = document.getElementById('preloader');
 window.addEventListener('load', function(){
   preload.style.display = 'none';
 })
+
+// setTimeout(() => {
+//   const preload = document.getElementById('preloader');
+//   preload.style.display = 'none';
+// }, 3000);
