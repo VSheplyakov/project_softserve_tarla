8a6bf302e81220b531e7533a51f596477ebfc732
DadaLV <vynohradova.l.a@gmail.com>
2022-08-11 21:18:58 +0300

    product servises changed

--- js/products-service.js
+++ js/products-service.js
@@ -1,19 +1,18 @@
 class ProductsService {
   constructor() {
-    if (!ProductsService._instance) ProductsService._instance = this;
-    return ProductsService._instance;
-}
-  
-async getProducts() {
-  if (!this.products) {
-    this.products = await( await fetch('api/products.json') ).json() ;
+      if (!ProductsService._instance) ProductsService._instance = this;
+      return ProductsService._instance;
   }
 
-  return this.products;
-}
+  async getProducts() {
+      if (!this.products) {
+          this.products = await( await fetch('api/products.json') ).json();
+      }
+      return this.products;
+  }
 
-async getProductById(id) {
-  const products = await this.getProducts();
-  return products.find(product => product.id === id);
-}
+  async getProductById(id) {
+      const products = await this.getProducts();
+      return products.find(product => product.id === id);
+  }
 }
