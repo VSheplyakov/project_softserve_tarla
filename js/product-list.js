fb8fb3f5ecb5157096ea1a2eda6fcacf3c1d405d
DadaLV <vynohradova.l.a@gmail.com>
2022-08-11 12:07:39 +0300

    productDomString created

--- js/product-list.js
+++ js/product-list.js
@@ -1,22 +1,22 @@
 class ProductList {
   constructor() {
-    this.container = document.querySelector('.shop__block');
+    this.container = document.querySelector(".shop__block");
     this.productsService = new ProductsService();
     this.renderProducts();
   }
   async renderProducts() {
     let productListDomString = "";
     const products = await this.productsService.getProducts();
-    products.forEach(product => {
-      productListDomString += this.createProductDomString(product)
+    products.forEach((product) => {
+      productListDomString += this.createProductDomString(product);
     });
   }
   createProductDomString() {
     return `<div class="shop_item">
-            <img src="images/${product.image}" alt="Croissant_1" class="shop_img">
+            <img src="images/${product.image}" alt="${product.title}" class="shop_img">
             <h3 class="item_name">All-Butter <br> Croissant</h3>
-            <span class="kit">Kit of 6 £12</span>
-            <a href="#" class="shop_btn btn">Add to cart</a>
-    </div>`
+            <span class="kit">Kit of 6 ${product.price}</span>
+            <a href="#" class="shop_btn btn data_id=${product.id}>Add to cart</a>
+    </div>`;
   }
 }
