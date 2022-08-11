34934ee9f3cff10549db863eddc0c718c5f8a6e6
DadaLV <vynohradova.l.a@gmail.com>
2022-08-11 21:18:40 +0300

    product list edited

--- js/product-list.js
+++ js/product-list.js
@@ -1,22 +1,25 @@
 class ProductList {
   constructor() {
-    this.container = document.querySelector(".shop__block");
-    this.productsService = new ProductsService();
-    this.renderProducts();
+      this.container = document.querySelector('.products-container');
+      this.ProductsService = new ProductsService();
+      this.renderProducts();
   }
   async renderProducts() {
-    let productListDomString = "";
-    const products = await this.productsService.getProducts();
-    products.forEach((product) => {
-      productListDomString += this.createProductDomString(product);
-    });
+      let productListDomString = '';
+      const products = await this.ProductsService.getProducts();
+      products.forEach(product => {
+          productListDomString += this.createProductDomString(product);
+      });
+      this.container.innerHTML = productListDomString;
+      //this.addEventListeners();
   }
   createProductDomString() {
     return `<div class="shop_item">
-            <img src="images/${product.image}" alt="${product.title}" class="shop_img">
-            <h3 class="item_name">All-Butter <br> Croissant</h3>
+            <img src="images/shop/${product.image}" alt="${product.title}" class="shop_img">
+            <h3 class="item_name">${product.title} <br> Croissant</h3>
             <span class="kit">Kit of 6 ${product.price}</span>
-            <a href="#" class="shop_btn btn data_id=${product.id}>Add to cart</a>
+            
     </div>`;
   }
 }
+new ProductList();
