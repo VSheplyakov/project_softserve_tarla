3239220fa23e72ccc4b572a1bfa26c4186ff93ef
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-08-11 23:46:14 +0300

    edited productList

--- js/product-list.js
+++ js/product-list.js
@@ -1,25 +1,37 @@
 class ProductList {
   constructor() {
-      this.container = document.querySelector('.products-container');
-      this.productsService = new ProductsService();
-      this.renderProducts();
+    this.container = document.querySelector('.products-container');
+    this.productsService = new ProductsService();
+    this.renderProducts();
   }
   async renderProducts() {
-      let productListDomString = '';
-      const products = await this.productsService.getProducts();
-      products.forEach(product => {
-          productListDomString += this.createProductDomString(product);
-      });
-      this.container.innerHTML = productListDomString;
-      //this.addEventListeners();
+    let productListDomString = '';
+    const products = await this.productsService.getProducts();
+    console.log(products);
+    products.forEach((product) => {
+      productListDomString += this.createProductDomString(product);
+    });
+    console.log(productListDomString);
+    this.container.innerHTML = productListDomString;
+    //this.addEventListeners();
   }
-  createProductDomString() {
+  createProductDomString(product) {
     return `<div class="shop_item">
             <img src="images/shop/${product.image}" alt="${product.title}" class="shop_img">
             <h3 class="item_name">${product.title} <br> Croissant</h3>
             <span class="kit">Kit of 6 ${product.price}</span>
             
     </div>`;
+  }
+  addEventListeners() {
+    document.querySelectorAll('.btn-buy').forEach((btn) => {
+      btn.addEventListener('click', this.addProductToCart.bind(this));
+    });
+  }
+  addProductToCart(event) {
+    const id = event.target.dataset.id;
+    const cart = new Cart();
+    cart.addProduct(id);
   }
 }
 new ProductList();
