9531fbd1154b9ab5446766bed2b4d6d49241ea77
DadaLV <vynohradova.l.a@gmail.com>
2022-08-12 13:34:52 +0300

    commented not in use

--- js/product-list.js
+++ js/product-list.js
@@ -7,13 +7,13 @@ class ProductList {
   async renderProducts() {
     let productListDomString = '';
     const products = await this.productsService.getProducts();
-    // console.log(products);
+    
     products.forEach((product) => {
       productListDomString += this.createProductDomString(product);
     });
-    // console.log(productListDomString);
+    
     this.container.innerHTML = productListDomString;
-    this.addEventListeners();
+    // this.addEventListeners();
   }
   createProductDomString(product) {
     return `<div class="shop_item">
@@ -23,15 +23,15 @@ class ProductList {
             <button class="shop_btn btn">Add to cart</button>
     </div>`;
   }
-  addEventListeners() {
-    document.querySelectorAll('.shop_btn').forEach((btn) => {
-      btn.addEventListener('click', this.addProductToCart.bind(this));
-    });
-  }
-  addProductToCart(event) {
-    const id = event.target.dataset.id;
-    const cart = new Cart();
-    cart.addProduct(id);
-  }
+  // addEventListeners() {
+  //   document.querySelectorAll('.shop_btn').forEach((btn) => {
+  //     btn.addEventListener('click', this.addProductToCart.bind(this));
+  //   });
+  // }
+  // addProductToCart(event) {
+  //   const id = event.target.dataset.id;
+  //   const cart = new Cart();
+  //   cart.addProduct(id);
+  // }
 }
 new ProductList();
