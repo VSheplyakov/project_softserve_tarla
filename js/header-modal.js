254cd44834f5c70efb75c57864dfbc32df51f983
DadaLV <vynohradova.l.a@gmail.com>
2022-08-13 22:20:04 +0300

    header modal changed

--- js/header-modal.js
+++ js/header-modal.js
@@ -5,10 +5,10 @@ function hide() {
 
 function showText () {
   let headerText;
-  if(window.innerWidth<1026){
-   headerText = "Check delivery";     
+  if(window.innerWidth>1026){
+   headerText = "We're focusing on London to start with, please enter your post code here to see if you're within our delivery zone!";     
  } else {
-   headerText = "We're focusing on London to start with, please enter your post code here to see if you're within our delivery zone!";
+   headerText = "Check delivery";
  }
  document.querySelector(".header_text").innerHTML = headerText;
 }
